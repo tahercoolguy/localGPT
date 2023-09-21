363de11b7c3d782578c5b2f2d584c43cf42ccf49
Taher Ali badnawarwala <tahercoolguy@gmail.com>
2023-09-21 13:42:31 +0530

    embedding is previous only to check if this is working or not

--- constants.py
+++ constants.py
@@ -38,7 +38,7 @@ DOCUMENT_MAP = {
 }
 
 # Default Instructor Model
-# EMBEDDING_MODEL_NAME = "hkunlp/instructor-large" # Uses 1.5 GB of VRAM (High Accuracy with lower VRAM usage)
+EMBEDDING_MODEL_NAME = "hkunlp/instructor-large" # Uses 1.5 GB of VRAM (High Accuracy with lower VRAM usage)
 
 ####
 #### OTHER EMBEDDING MODEL OPTIONS
@@ -53,7 +53,7 @@ DOCUMENT_MAP = {
 #### MULTILINGUAL EMBEDDING MODELS
 ####
 
-EMBEDDING_MODEL_NAME = "intfloat/multilingual-e5-large" # Uses 2.5 GB of VRAM 
+# EMBEDDING_MODEL_NAME = "intfloat/multilingual-e5-large" # Uses 2.5 GB of VRAM 
 # EMBEDDING_MODEL_NAME = "intfloat/multilingual-e5-base" # Uses 1.2 GB of VRAM 
 
 
